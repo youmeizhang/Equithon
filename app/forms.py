97b7af94bcf01879b93527ed3649587577ce2f47
Lindsey Jin <l44jin@uwaterloo.ca>
2018-05-05 22:45:13 -0400

    updated forms

--- app/forms.py
+++ app/forms.py
@@ -1,17 +1,30 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField, IntegerField
 from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign In')
+    submit = SubmitField('Log In')
 
 
 class SignupForm(FlaskForm):
-        username = StringField('Username', validators=[DataRequired()])
+        first_name = StringField('Your Name', validators=[DataRequired()])
+        last_name = StringField('Your Name', validators=[DataRequired()])
+        email = StringField('Email', validators=[DataRequired()])
         password = PasswordField('Password', validators=[DataRequired()])
-        remember_me = BooleanField('Remember Me')
-        submit = SubmitField('Sign In')
+        submit = SubmitField('Sign Up')
+
+
+class ProfileForm(FlaskForm):
+        school = StringField('Your Name', validators=[DataRequired()])
+        grade = StringField('Grade', validators=[DataRequired()])
+        age = StringField('Age', validators=[DataRequired()])
+        phone = IntegerField('Phone Number (optional)')
+        address = StringField('Address (optional)')
+        subjects = SelectField('Subjects', validators=[DataRequired()])
+        avail_days = SelectField('Available Days of the Week', validators=[DataRequired()])
+        blurb = StringField('Tell Us About Yourself! (optional)')
+        submit = SubmitField('Submit')
--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from flask import render_template, flash, redirect
 from app import app
 from app.forms import LoginForm
 from app.forms import SignupForm
+from app.forms import ProfileForm
 
 
 @app.route('/')
@@ -37,6 +38,15 @@ def signup():
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
-        return redirect('/index')
+        return redirect('/profile')
     return render_template('signup.html', title='Signup', form=form)
+
+
+@app.route('/profile', methods=['GET', 'POST'])
+def profile():
+    form = ProfileForm()
+    if form.validate_on_submit():
+        flash('This works gr8')
+        return redirect('/index')
+    return render_template('profile.html', title='Profile', form=form)
 
